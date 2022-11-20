7742a0ed537a1db2c04e2e569efa009112866b7f
Kakadu <Kakadu@pm.me>
2022-11-20 15:19:11 +0300

    Comments and polishing
    Signed-off-by: Kakadu <Kakadu@pm.me>

---
+++ lib/lib.mli
@@ -0,0 +1,129 @@
+(** An implementation of miniKanren as a library *)
+
+(** Syntactic terms. Non-typed *)
+module Term : sig
+  type t =
+    | Var of string (** Logic variables with name *)
+    | Symbol of string (** Scheme's symbol *)
+    | Cons of t * t (* List's cons cell *)
+    | Nil (** Empty list *)
+
+  (** {3} Smart constructors *)
+
+  val cons : t -> t -> t
+  val symbol : string -> t
+  val var : string -> t
+
+  (** {3} Printing *)
+  val pp : Format.formatter -> t -> unit
+end
+
+(** A goal -- something that can be calculated relationally *)
+type goal =
+  | Unify of Term.t * Term.t (** Unification *)
+  | Conj of goal list (** Conjunction *)
+  | Conde of goal list (** (interleaved) Disjunction *)
+  | Fresh of string * goal (** Creating of fresh (existential) variables *)
+  | Call of string * Term.t list (** Call to another relation with arguments *)
+  | TraceSVars of string list (** Tracing of some variables. Only for debugging *)
+
+(** A combinator to create many fresh variables instantly *)
+val fresh : string list -> goal -> goal
+
+val pp_goal : Format.formatter -> goal -> unit
+
+(** Substitution stores binding from logic variables to logic values *)
+module Subst : sig
+  type key = int
+  type 'a t = 'a Map.Make(Int).t
+
+  val pp : (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a t -> unit
+end
+
+(** Runtime logic values *)
+module Value : sig
+  type t =
+    | Var of int
+    | Symbol of string
+    | Cons of t * t
+    | Nil
+
+  (** {3} Smart constructors *)
+
+  val var : int -> t
+  val symbol : string -> t
+  val cons : t -> t -> t
+  val nil : t
+  val _0 : t
+  val _1 : t
+
+  (** Walking via substitution *)
+
+  (** Call [walk subst v] replace all occurences of variables from [dom subst] by [subst(v)].
+      The corner case is when variable [v] is going to be replaced by something that contains this [v].
+      In this situation an ``Occurs check'' should happen and give an error, but it's currently not implemented
+      *)
+  val walk : t Subst.t -> t -> t
+
+  (** {3} Printing *)
+
+  val pp : Format.formatter -> t -> unit
+  val ppw : t Subst.t -> Format.formatter -> t -> unit
+end
+
+(** A substitution. We will have only substitutions of logic variables (i.e. it's indexes) to Value.t *)
+type subst = Value.t Subst.t
+
+(** Unification [unify subst a b] unifies [a] and [b] or gives an error. Commutative. *)
+val unify : subst -> Value.t -> Value.t -> subst option
+
+module State : sig
+  type t
+
+  val empty : t
+  val add_var : string -> Value.t -> t -> t
+  val ( --> ) : string -> Value.t -> t -> t
+  val add_var_logic : int -> Value.t -> t -> t
+  val ( ->> ) : int -> Value.t -> t -> t
+  val add_rel : string -> string list -> goal -> t -> t
+  val add_rel1 : string * string list * goal -> t -> t
+end
+
+type st = State.t
+
+type error =
+  [ `BadArity
+  | `UnboundRelation of string
+  | `UnboundSyntaxVariable of string
+  ]
+
+val pp_error
+  :  Format.formatter
+  -> [< `BadArity | `UnboundRelation of string | `UnboundSyntaxVariable of string ]
+  -> unit
+
+val failwiths : ('a, Format.formatter, unit, 'b) format4 -> 'a
+
+(** A monad module, to implement a miniKanren interpreter *)
+module StateMonad : sig
+  type ('a, 'b) t
+
+  (** Main function to run something *)
+  val run : ('st, 'r) t -> 'st -> ('r, error) result
+end
+
+type 'a state = (st, 'a) StateMonad.t
+
+(** A stream of answers of calculation *)
+module Stream : sig
+  type 'a t
+
+  val take : ?n:int -> 'a t -> 'a list
+end
+
+val eval
+  :  ?trace_svars:bool
+  -> ?trace_uni:bool
+  -> ?trace_calls:bool
+  -> goal
+  -> subst Stream.t state
--- lib/test_numbers.ml
+++ lib/test_numbers.ml
@@ -1,3 +1,5 @@
+(** A relational implementation of Oleg-style arithmetic *)
+
 open Lib
 open Format
 
@@ -14,8 +16,6 @@ let run_optimistically
   | Result.Error e -> failwiths "Error: %a" pp_error e
 ;;
 
-let default_env = State.empty
-
 let rec build_num n =
   let open Term in
   if n mod 2 = 1
@@ -27,90 +27,114 @@ let rec build_num n =
     Cons (Symbol "0", build_num (n / 2)))
 ;;
 
-let default_env =
+let poso = "poso", [ "n" ], fresh [ "a"; "d" ] (Unify (Var "n", Cons (Var "a", Var "d")))
+
+let gt1o =
+  let open Term in
+  ( "gt1o"
+  , [ "n" ]
+  , fresh
+      [ "a"; "ad"; "dd" ]
+      (Unify (var "n", cons (var "a") (cons (var "ad") (var "dd")))) )
+;;
+
+let full_addero =
   let open Term in
   let ( == ) a b = Unify (a, b) in
   let _0 = Symbol "0" in
   let _1 = Symbol "1" in
-  default_env
-  |> State.add_rel
-       "poso"
-       [ "n" ]
-       (fresh [ "a"; "d" ] (Unify (Var "n", Cons (Var "a", Var "d"))))
-  |> State.add_rel
-       "gt1o"
-       [ "n" ]
-       (fresh
-          [ "a"; "ad"; "dd" ]
-          (Unify (var "n", cons (var "a") (cons (var "ad") (var "dd")))))
-  |> State.add_rel
-       "full-addero"
-       [ "b"; "x"; "y"; "r"; "c" ]
-       (let b, x, y, r, c = Var "b", Var "x", Var "y", Var "r", Var "c" in
-        Conde
-          [ Conj [ _0 == b; _0 == x; _0 == y; _0 == r; _0 == c ]
-          ; Conj [ _1 == b; _0 == x; _0 == y; _1 == r; _0 == c ]
-          ; Conj [ _0 == b; _1 == x; _0 == y; _1 == r; _0 == c ]
-          ; Conj [ _1 == b; _1 == x; _0 == y; _0 == r; _1 == c ]
-          ; Conj [ _0 == b; _0 == x; _1 == y; _1 == r; _0 == c ]
-          ; Conj [ _1 == b; _0 == x; _1 == y; _0 == r; _1 == c ]
-          ; Conj [ _0 == b; _1 == x; _1 == y; _0 == r; _1 == c ]
-          ; Conj [ _1 == b; _1 == x; _1 == y; _1 == r; _1 == c ]
-          ])
-  |> State.add_rel
-       "addero"
-       [ "d"; "n"; "m"; "r" ]
-       (let d, n, m, r, a, c = Var "d", Var "n", Var "m", Var "r", Var "a", Var "c" in
-        Conde
-          [ Conj [ _0 == d; Nil == m; n == r ]
-          ; Conj [ _0 == d; Nil == n; m == r; Call ("poso", [ m ]) ]
-          ; Conj [ _1 == d; Nil == m; Call ("addero", [ _0; n; cons _1 Nil; r ]) ]
-          ; Conj
-              [ _1 == d
-              ; Nil == n
-              ; Call ("poso", [ m ])
-              ; Call ("addero", [ _0; m; cons _1 Nil; r ])
-              ]
-          ; Conj
-              [ cons _1 Nil == n
-              ; cons _1 Nil == m (* ; TraceSVars [ "d"; "n"; "m"; "r" ] *)
-              ; fresh
-                  [ "a"; "c" ]
-                  (Conj [ r == cons a c; Call ("full-addero", [ d; _1; _1; a; c ]) ])
-              ]
-          ; Conj
-              [ cons _1 Nil == n (* ; TraceSVars [ "d"; "m"; "r" ] *)
-              ; Call ("gen-addero", [ d; n; m; r ])
-              ]
-          ; Conj
-              [ cons _1 Nil == m
-              ; Call ("gt1o", [ n ])
-              ; Call ("gt1o", [ r ])
-              ; Call ("addero", [ d; cons _1 Nil; n; r ])
-              ]
-          ; Conj [ Call ("gt1o", [ n ]); Call ("gen-addero", [ d; n; m; r ]) ]
-          ])
-  |> State.add_rel
-       "gen-addero"
-       [ "d"; "n"; "m"; "r" ]
-       (fresh
-          [ "u"; "b"; "c"; "e"; "x"; "y"; "z" ]
-          (* TODO: renaming b -> v kind of helps but maybe there is an issue with clashing names somewhere else *)
-          (Conj
-             [ cons (var "u") (var "x") == var "n"
-             ; cons (var "b") (var "y") == var "m"
-             ; Call ("poso", [ Var "y" ])
-             ; cons (var "c") (var "z") == var "r"
-             ; Call ("poso", [ Var "z" ])
-             ; TraceSVars [ "b" ]
-             ; Call ("full-addero", [ Var "d"; Var "u"; Var "b"; Var "c"; Var "e" ])
-             ; TraceSVars [ "c"; "e" ]
-             ; Call ("addero", [ Var "e"; Var "x"; Var "y"; Var "z" ])
-             ]))
-  |> State.add_rel
-       "pluso"
-       [ "n"; "m"; "k" ]
-       (Call ("addero", [ _0; Var "n"; Var "m"; Var "k" ]))
+  ( "full-addero"
+  , [ "b"; "x"; "y"; "r"; "c" ]
+  , let b, x, y, r, c = Var "b", Var "x", Var "y", Var "r", Var "c" in
+    Conde
+      [ Conj [ _0 == b; _0 == x; _0 == y; _0 == r; _0 == c ]
+      ; Conj [ _1 == b; _0 == x; _0 == y; _1 == r; _0 == c ]
+      ; Conj [ _0 == b; _1 == x; _0 == y; _1 == r; _0 == c ]
+      ; Conj [ _1 == b; _1 == x; _0 == y; _0 == r; _1 == c ]
+      ; Conj [ _0 == b; _0 == x; _1 == y; _1 == r; _0 == c ]
+      ; Conj [ _1 == b; _0 == x; _1 == y; _0 == r; _1 == c ]
+      ; Conj [ _0 == b; _1 == x; _1 == y; _0 == r; _1 == c ]
+      ; Conj [ _1 == b; _1 == x; _1 == y; _1 == r; _1 == c ]
+      ] )
+;;
+
+let addero =
+  let open Term in
+  let ( == ) a b = Unify (a, b) in
+  let _0 = Symbol "0" in
+  let _1 = Symbol "1" in
+  ( "addero"
+  , [ "d"; "n"; "m"; "r" ]
+  , let d, n, m, r, a, c = Var "d", Var "n", Var "m", Var "r", Var "a", Var "c" in
+    Conde
+      [ Conj [ _0 == d; Nil == m; n == r ]
+      ; Conj [ _0 == d; Nil == n; m == r; Call ("poso", [ m ]) ]
+      ; Conj [ _1 == d; Nil == m; Call ("addero", [ _0; n; cons _1 Nil; r ]) ]
+      ; Conj
+          [ _1 == d
+          ; Nil == n
+          ; Call ("poso", [ m ])
+          ; Call ("addero", [ _0; m; cons _1 Nil; r ])
+          ]
+      ; Conj
+          [ cons _1 Nil == n
+          ; cons _1 Nil == m (* ; TraceSVars [ "d"; "n"; "m"; "r" ] *)
+          ; fresh
+              [ "a"; "c" ]
+              (Conj [ r == cons a c; Call ("full-addero", [ d; _1; _1; a; c ]) ])
+          ]
+      ; Conj
+          [ cons _1 Nil == n (* ; TraceSVars [ "d"; "m"; "r" ] *)
+          ; Call ("gen-addero", [ d; n; m; r ])
+          ]
+      ; Conj
+          [ cons _1 Nil == m
+          ; Call ("gt1o", [ n ])
+          ; Call ("gt1o", [ r ])
+          ; Call ("addero", [ d; cons _1 Nil; n; r ])
+          ]
+      ; Conj [ Call ("gt1o", [ n ]); Call ("gen-addero", [ d; n; m; r ]) ]
+      ] )
+;;
+
+let gen_addero =
+  let open Term in
+  let ( == ) a b = Unify (a, b) in
+  let _0 = Symbol "0" in
+  let _1 = Symbol "1" in
+  ( "gen-addero"
+  , [ "d"; "n"; "m"; "r" ]
+  , fresh
+      [ "u"; "b"; "c"; "e"; "x"; "y"; "z" ]
+      (* TODO: renaming b -> v kind of helps but maybe there is an issue with clashing names somewhere else *)
+      (Conj
+         [ cons (var "u") (var "x") == var "n"
+         ; cons (var "b") (var "y") == var "m"
+         ; Call ("poso", [ Var "y" ])
+         ; cons (var "c") (var "z") == var "r"
+         ; Call ("poso", [ Var "z" ])
+         ; TraceSVars [ "b" ]
+         ; Call ("full-addero", [ Var "d"; Var "u"; Var "b"; Var "c"; Var "e" ])
+         ; TraceSVars [ "c"; "e" ]
+         ; Call ("addero", [ Var "e"; Var "x"; Var "y"; Var "z" ])
+         ]) )
+;;
+
+let pluso =
+  let open Term in
+  let _0 = Symbol "0" in
+  let _1 = Symbol "1" in
+  "pluso", [ "n"; "m"; "k" ], Call ("addero", [ _0; Var "n"; Var "m"; Var "k" ])
+;;
+
+let default_env =
+  State.empty
+  |> State.add_rel1 poso
+  |> State.add_rel1 gt1o
+  |> State.add_rel1 full_addero
+  |> State.add_rel1 addero
+  |> State.add_rel1 gen_addero
+  |> State.add_rel1 pluso
 ;;
 
 let%expect_test _ =
@@ -146,7 +170,8 @@ let%expect_test _ =
          goal
          Value.pp
          (Value.walk st (Var 10)));
-  [%expect {|
+  [%expect
+    {|
     Running: (pluso (cons '1 '()) (cons '0 (cons '1 '()))
     q)
     Result: (pluso (cons '1 '()) (cons '0 (cons '1 '()))
